44cb06d71ccfa4412753db7235ab66c01007a78c
John Mohlenkamp <john@mohlenkamp.com>
2021-05-04 16:11:33 -0500

    updated index with new tags

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+var taskIdCounter = 0;
+
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
@@ -26,6 +28,8 @@ var createTaskEl = function (taskDataObj) {
   // create list item
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
+
+  listItemEl.setAttribute("data-task-id",taskIdCounter)
 
   // create div to hold task info and add to list item
   var taskInfoEl = document.createElement("div");
